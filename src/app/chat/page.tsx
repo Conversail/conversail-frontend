951dd0f370217dd2c6f69be5297e5c431543ee50
eduardor0cha <c.eduardo_rn@outlook.com>
2023-06-08 18:56:03 -0300

    feat: add typing status management

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,13 +3,22 @@
 import { Header, Message, ToggleThemeButton } from "@/src/components";
 import { useSocket } from "@/src/hooks/useSocket";
 import { MessageType } from "@/src/types/message";
-import { useCallback, useEffect, useRef, useState } from "react";
+import {
+  ChangeEvent,
+  FormEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { BsBoxArrowLeft, BsPlug, BsSend } from "react-icons/bs";
 
 export default function Chat() {
   const [status, setStatus] = useState<"lazy" | "pairing" | "paired">("lazy");
   const [messages, setMessages] = useState<MessageType[]>([]);
   const [chatEnded, setChatEnded] = useState<boolean>(false);
+  const [isUserTyping, setIsUserTyping] = useState<boolean>(false);
+  const [isMateTyping, setIsMateTyping] = useState<boolean>(false);
   const socket = useSocket(process.env.NEXT_PUBLIC_API_URL ?? "");
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -30,6 +39,14 @@ export default function Chat() {
           ...messages,
         ]);
       });
+
+      socket.on("typing", () => {
+        setIsMateTyping(true);
+      });
+
+      socket.on("stop typing", () => {
+        setIsMateTyping(false);
+      });
     }
   }, [socket, setStatus, messages, setMessages, setChatEnded]);
 
@@ -48,28 +65,49 @@ export default function Chat() {
     }
   }, [status, setStatus, socket]);
 
-  const handleSendMessage = useCallback(() => {
-    if (!inputRef.current) return;
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
 
-    const content = inputRef.current.value.trim();
-    if (!content) return;
-    const message: MessageType = {
-      content,
-      sentAt: new Date(),
-      fromYourself: true,
-      replyTo: "0",
-    };
+      if (!isUserTyping) return;
+      if (!inputRef.current) return;
 
-    socket?.emit(
-      "message",
-      { content, sentAt: new Date() },
-      (response: string) => {
-        message.id = response;
+      const content = inputRef.current.value.trim();
+      if (!content) return;
+      const message: MessageType = {
+        content,
+        sentAt: new Date(),
+        fromYourself: true,
+        replyTo: "0",
+      };
+
+      socket?.emit(
+        "message",
+        { content, sentAt: new Date() },
+        (response: string) => {
+          message.id = response;
+        }
+      );
+      setMessages([message, ...messages]);
+      inputRef.current.value = "";
+      socket?.emit("stop typing");
+      setIsUserTyping(false);
+    },
+    [inputRef, setMessages, messages, socket, isUserTyping]
+  );
+
+  const handleInputChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.value.trim() === "") {
+        socket?.emit("stop typing");
+        setIsUserTyping(false);
+      } else {
+        socket?.emit("typing");
+        setIsUserTyping(true);
       }
-    );
-    setMessages([message, ...messages]);
-    inputRef.current.value = "";
-  }, [inputRef, setMessages, messages, socket]);
+    },
+    [socket]
+  );
 
   return (
     <div className="p-chat">
@@ -81,19 +119,22 @@ export default function Chat() {
           <div className="p-chat__messages-area">
             <div className="p-chat__messages-area__warnings">
               {status === "pairing" ? (
-                <span className="p-chat__pairing-indicator">Pairing</span>
+                <span className="p-chat__indicator-with-ellipsis">Pairing</span>
               ) : null}
               {status === "paired" && messages.length === 0 ? (
                 <span>Paired</span>
               ) : null}
               {chatEnded ? <span>Chat ended</span> : null}
+              {isMateTyping ? (
+                <span className="p-chat__indicator-with-ellipsis">Typing</span>
+              ) : null}
             </div>
             {messages.map((message, i) => (
               <Message {...message} key={i} />
             ))}
           </div>
           <div className="p-chat__message-bar">
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form onSubmit={(e) => handleSubmit(e)}>
               <button
                 className="p-chat__bar-button"
                 onClick={() => handlePair()}
@@ -109,14 +150,18 @@ export default function Chat() {
                 placeholder="Message"
                 className="p-chat__message-input"
                 ref={inputRef}
+                disabled={status !== "paired"}
+                onChange={(e) => handleInputChange(e)}
               />
-              <button
-                className="p-chat__bar-button"
-                onClick={() => handleSendMessage()}
-                type="submit"
-              >
-                <BsSend className="p-chat__bar-button__icon" />
-              </button>
+              {isUserTyping ? (
+                <button
+                  className="p-chat__bar-button p-chat__send-button"
+                  type="submit"
+                  disabled={status !== "paired"}
+                >
+                  <BsSend className="p-chat__bar-button__icon" />
+                </button>
+              ) : null}
             </form>
           </div>
         </div>
